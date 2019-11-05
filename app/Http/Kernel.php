6985437bbd10f0ab8f0c5a072eebd755dab765e4
Ryan Freeman <info@ryanfreeman.io>
2019-11-05 20:25:03 +0000

    Added headers middleware to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,7 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             'bindings',
             \App\Http\Middleware\Logging::class,
+            'httpHeaders:Come Work For US!',
         ],
     ];
 
@@ -62,6 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'httpHeaders' => \App\Http\Middleware\HttpHeaders::class,
     ];
 
     /**
